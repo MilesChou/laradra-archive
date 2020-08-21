f6311a92dd9b4cd5c002354dd73bc079175d3180
MilesChou <jangconan@gmail.com>
2020-08-21 15:18:08 +0800

    Initial project

--- src/Sample.php
+++ src/Sample.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace MilesChou\Template;
+namespace MilesChou\Laradra;
 
 class Sample
 {
--- tests/Unit/SampleTest.php
+++ tests/Unit/SampleTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
-use MilesChou\Template\Sample;
+use MilesChou\Laradra\Sample;
 use Tests\TestCase;
 
 class SampleTest extends TestCase
